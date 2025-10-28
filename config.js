bddc456f187a39c04bf228fbf337992e6d107488
ahmadfreefire879-web <ahmadfreefire879@gmail.com>
2025-10-28 17:17:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SMD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR0lXSnVtNm9MYVNVUUpNK04vR0NlaW9HWk00QWlNc1BZMTRXMFBpSTFsUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidkRHbU5udDFqQnJwRXpiN283TXhkN1V6Rkx0WVlHZzhpZldEQnZGbzdEQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnUHgxS0FJYzdvUTJUNjA3VjZPaW5taWxNa0xHM0FEbTdROTloQkpVckc0PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4Qm14SlNsRXZNRXRVcE1aSHB2ZXpVL3NuK3FUWXR6UVhyQ3dqNFZneENRPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InVNZG9JNGVRYlZ1WEJGZU9zV082SUw3cVoyT0ZKbkJHdnpvRDUzNm5FMzQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkxZZHRhRit1RDgzK3oyNTlSR1QvRTYvU0xqMHBhZ1NoSUhFYnZhWGIveWM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUxEV0VPSTFXQm4wVFdMRnJxVnRRZFFldjN2TS84Nmkvdy9nbHlCM0NHST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZFhFM0NmTngyMzEzdmRPSWtvd1Q2eVU4bEIrbTFwWGVqbEVick1RbEJnYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkVweE0xQklncm44M01IZjFDbmJVMitKN0l0d3dyK0hXeUVlMTlrVnhDU3M4ak9ZRFZFL3JaN1ExdXRwMlN5ODZGNGlQdjZKWjE0Zlo1OStNbm5sZER3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjE1LCJhZHZTZWNyZXRLZXkiOiJNbVdIR05kdG9GMHlBM2xua29KM084SGdqYUUvb3lDWXZPVUM1Q0VyeXNJPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjkyMzQ1NzAxMDcwNEBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiJBQ0QwMkVFRjU0MTNBNDkyODVBQ0QyMEQ3QjM3MTJCRiJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzYxNjUzNjI5fSx7ImtleSI6eyJyZW1vdGVKaWQiOiI5MjM0NTcwMTA3MDRAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiQUNCQTUxOUExMzk3QkU2MUJFQTFFNjIwRTAyOEQxOUMifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc2MTY1MzYzMH1dLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiUEdNUllDWlkiLCJtZSI6eyJpZCI6IjkyMzQ1NzAxMDcwNDo0MEBzLndoYXRzYXBwLm5ldCIsImxpZCI6IjIyMzY3ODIwNTc5NjM5Njo0MEBsaWQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0xhQ3YrUUNFUFhtZ3NnR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Ikh3eFByMHBNbDhCZXFMeFdXQ2dKZVowaWx5elFOUjVNd2E3c1oyLzZzUTA9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlV6WnppMW1PRUp3NE5wQ25wYmNlK1c0RkpvOHJ4U0p3NmhIRThKb0FjaW5FMElvc2pIV3RqVlVVL2svbVVnOHB2WXZSMC9JOTRBVlkyNXdjd2Q3ekNnPT0iLCJkZXZpY2VTaWduYXR1cmUiOiIycWx0UHZvSSt1SUU1WXVMc1VFWlJGVzRwems1NWljK0xNWFRpb3V4ZzR6dkFUdEh3blR2b2xUWmNycVBVcDNXdjBFWUZWUmJ4N01GTm94T2VaMFRBdz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzQ1NzAxMDcwNDo0MEBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJSOE1UNjlLVEpmQVhxaThWbGdvQ1htZElwY3MwRFVlVE1HdTdHZHYrckVOIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwicm91dGluZ0luZm8iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQklJQ0E9PSJ9LCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3NjE2NTM2MjUsImxhc3RQcm9wSGFzaCI6IjJWNzdxVSIsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBQ2dxIn0=",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW Sʜᴀʙᴀɴ-Mᴅ 𓅓*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SᴇᴇN YᴏᴜʀE SᴛᴀᴛᴜS JᴜsT NᴏW AHMII MD-Mᴅ 𓅓*",
 // set the auto reply massage on status reply
 AUTO_CHATBOT: process.env.AUTO_CHATBOT || "false", 
 // "true" = default ON, "false" = default OFF
@@ -25,7 +25,7 @@ PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://ik.imagekit.io/mrshaban/Picsart_25-02-01_22-47-44-239.jpg",
 // add custom menu and mention reply image url   
-BOT_NAME: process.env.BOT_NAME || "SHABAN-MD",
+BOT_NAME: process.env.BOT_NAME || "E3_HACKER_MD",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "SHABAN-MD",
 // type sticker pack name 
@@ -35,7 +35,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself  
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923303788282",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝐌𝐑 𝐒𝐇𝐀𝐁𝐀𝐍",
+OWNER_NAME: process.env.OWNER_NAME || "E3-AHMII-MD",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ*",
 // add bot owner name    
@@ -43,7 +43,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/mrshaban/Picsart_25-
 // add img for alive msg 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -51,7 +51,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups  
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
@@ -67,6 +67,6 @@ ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
 // change it to 'same' if you want to resend deleted message in same chat
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
 // make it true for auto recoding 
 };
